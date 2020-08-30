5d723f333772ce02a94a10a8f345ae08b90dac85
chandru415 <chandru9052@gmail.com>
2020-08-30 12:21:41 +0530

    [chandu] Project Setup

---
+++ CQRS-FluentValidation/API/Installers/InstallServices/SwaggerInstaller.cs
@@ -0,0 +1,18 @@
+﻿using API.Installers.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Installers.InstallServices
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CQRS With MediatR APIs", Version = "v1" });
+            });
+        }
+    }
+}
